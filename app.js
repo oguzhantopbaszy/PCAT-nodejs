7eb4b34e4183fbd1ed3462eb7fd95f2eb5930fec
oguzhantopbaszy <oguzhantopbas@zyelektrikli.com>
2022-07-18 15:33:03 +0300

    Nodemon and Express installed. Port test completed

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const fn = function () {
-  console.log("It's working");
-};
+const express = require('express')
 
-fn();
+const app = express();
+
+app.get('/', (req, res) => {
+
+  const photo = {
+    id:1,
+    name: "Photo name",
+    description: "Photo description "
+  }
+  res.send(photo)
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`)
+})
