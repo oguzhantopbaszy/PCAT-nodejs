9f132752d514f843d86a9c297e9b300c0483ffe2
oguzhantopbaszy <oguzhantopbas@zyelektrikli.com>
2022-07-29 14:58:03 +0300

    Model and Dynamic Content completed

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const ejs = require('ejs')
 const path = require('path')
+const Photo = require('./models/Photo')
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 //Template Engine
 app.set("view engine", "ejs");
@@ -13,8 +22,11 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 //Routes
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  })
 })
 
 app.get('/about', (req, res) => {
@@ -25,8 +37,8 @@ app.get('/add', (req, res) => {
   res.render('add')
 })
 
-app.post('/photos', (req, res) => {
-  console.log(req.body);
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
   res.redirect('/')
 })
 
