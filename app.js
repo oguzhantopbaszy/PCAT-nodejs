f08783c86a6fd3d3329703ea0b4f0970b487fc10
oguzhantopbaszy <oguzhantopbas@zyelektrikli.com>
2022-08-18 13:13:46 +0300

    Delete photo completed

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 //connect DB
 mongoose.connect('mongodb://localhost/pcat-test-db', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 })
 
 //Template Engine
@@ -24,7 +24,9 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(fileUpload())
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method', {
+  methods: ['POST', 'GET'] //Hangi methodları override edeceğini belirtmek için yazdık.
+}))
 
 //Routes
 app.get('/', async (req, res) => {
@@ -91,6 +93,14 @@ app.put('/photos/:id', async (req,res) => {
 
   res.redirect(`/photos/${req.params.id}`)
 
+})
+
+app.delete('/photos/:id', async (req,res) => {
+  const photo = await Photo.findOne({_id: req.params.id})
+  let deletedImage = __dirname + "/public" + photo.image
+  fs.unlinkSync(deletedImage)
+  await Photo.findByIdAndRemove(req.params.id)
+  res.redirect('/')
 })
 
 const port = 3000;
